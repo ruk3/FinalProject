14ab85426cd1fea43fafe5ec56a164cbd539283c
jaytailor12 <56904595+jaytailor12@users.noreply.github.com>
2020-05-12 04:58:38 -0400

    FOV view
    Shows the FOV angle for the capsule in the tag scene.

---
+++ Forces Expanded/Assets/Scripts/sight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public float Angle;
+    public float Radius;
+    private void OnDrawGizmos()
+    {
+        //creates the border for the field of view
+        Vector3 FOVBorder1 = Quaternion.AngleAxis(Angle, transform.up) * transform.forward*Radius;
+        Vector3 FOVBorder2 = Quaternion.AngleAxis(-Angle, transform.up) * transform.forward*Radius;
+
+
+        //shows the fov angle
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, FOVBorder1);
+        Gizmos.DrawRay(transform.position, FOVBorder2);
+
+        //To be continued checking if in the FOV
+
+    }
+}
